2d3691d7b51106d349282cf13aedd865030ec4cb
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-09-23 11:30:10 -0700

    setting up api routes

---
+++ routes/api/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+// GET /notes should read the db.json file and return all saved notes as JSON.
+
+
+
+// POST /notes should receive a new note to save on the request body, 
+// add it to the db.json file,
+// then return the new note to the client
+
+
+
+// DELETE /notes/:id should receive a query parameter containing the id of a note to delete
+
+
+module.exports = router; 
