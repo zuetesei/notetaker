f6b76cae1bcfe59acedd66c9d1e92adc6b1f75df
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-09-25 15:15:29 -0700

    testing the api route that reads the db.json and returns all saved notes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const fs = require('fs');
+const db = require('../../db/db.json');
+
 
 // GET /notes should read the db.json file and return all saved notes as JSON.
-
+router.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        console.log(JSON.parse(data));
+        res.json(data)
+    })
+})
 
 
 // POST /notes should receive a new note to save on the request body, 
@@ -13,4 +22,4 @@ const router = require('express').Router();
 // DELETE /notes/:id should receive a query parameter containing the id of a note to delete
 
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 // dependencies 
 const express = require('express');
-const apiRoutes = require('./routes/api');
-const htmlRoutes = require('./routes/html');
-
-// setting up the server 
+const path = require('path');
 const app = express();
+// sets port for listening, heroku decides, if not, use port 3001 
 const PORT = process.env.PORT || 3001;
 
-// middleware - connecting js and css files
-app.use(express.static('./public'));
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+const apiRoutes = require('./routes/api');
+// const htmlRoutes = require('./routes/html');
+
+// this middleware says to CSS and JS files directory named public 
+app.use(express.static('public')); // tested 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(apiRoutes);
+// app.use(htmlRoutes);
 
 app.listen(PORT, () =>
     console.log(`Express server listening on port ${PORT}!`)
